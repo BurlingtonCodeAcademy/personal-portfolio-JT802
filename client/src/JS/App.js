470e2e94f2748156259b8a297cc2f139a749d004
JT802 <jonathan.tresch@gmail.com>
2020-10-15 17:07:03 -0400

    Friday evening bones

--- client/src/JS/App.js
+++ client/src/JS/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
       <h1>Hello portfolio!</h1>
