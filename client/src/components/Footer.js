5f96154925f735c8d0ae17235f798fded36b29dd
JT802 <jonathan.tresch@gmail.com>
2020-11-06 14:22:31 -0500

    Friday work toward resubmit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,20 +9,22 @@ function Footer() {
             </div>
             <div> {/* Olivia helped me get this footer laid out satisfactorily. I found it to be the toughest part of this project. */}
                 <form className="form-wrapper">
+                    <div className="contact">
                     <input id="name" type="text" placeholder="Name" />
                     <input id="email" type="text" placeholder="Email" />
-                    <textarea id="textarea" placeholder="Message"></textarea>
+                    <textarea id="textarea" maxLength="500" cols="30" rows="15" placeholder="Message"></textarea>
                     <button id="submitButton" type="button">Submit</button>
+                    </div>
                     {/* What if someone wants to actually use this form to get in touch with me? */}
                     {/* How can I make the Submit button actually send the information somewhere? */}
                 </form>
             </div> {/* Thanks to GJ for providing the social-media icons from an earlier project. */}
             <div id='socialMedia'>
                 <a href='/'><img src='../Icons/home.png' id='burly' alt='' />Burlington, Vermont</a>
-                <a href='mailto:jonathan.tresch@gmail.com'><img src='../Icons/email.png' id='eMail' alt='' />Email Me</a>
+                <a href='mailto:jonathan.tresch@gmail.com'><img src='../Icons/email.png' id='mail' alt='' />Email Me</a>
                 <a href='/'><img src='../Icons/phone.png' id='phone' alt='' />Via railroad semaphore</a>
                 <a href='https://www.instagram.com/3rdrailrunner'><img src='../Icons/instagram.png' id='instagram' alt='' />Instagram</a>
-                <a href='https://www.facebook.com/jonathan.tresch'><img src='../Icons/facebook.png' id='effBee' alt='' />Facebook</a>
+                <a href='https://www.facebook.com/jonathan.tresch'><img src='../Icons/facebook.png' id='effbee' alt='' />Facebook</a>
                 <a href='https://www.twitter.com/PhoenixDev802'><img src='../Icons/twitter.png' id='bird' alt='' />Twitter</a>
             </div>
             <div id='copyright'> {/* In case Bob or the TAs wonder, Eli Crow is my partner, and he wanted to contribute to this project. */}
